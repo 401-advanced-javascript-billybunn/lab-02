c3b8eff59f7844e7cb97239064b181f9971205c8
Billy Bunn <williammbunn@gmail.com>
2019-03-19 22:46:43 -0700

    Adds flyingvehicle constructor module

---
+++ objects/flyingvehicle-constructor.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const FlyingVehicle = function (name) {
+  this.name = name;
+  this.canFly = true;
+};
+
+FlyingVehicle.prototype.takeOff = () => {
+  return 'We have lift-off!';
+};
+
+FlyingVehicle.prototype.crash = () => {
+  return 'WE\'RE ALL GONNA DIE!';
+};
+
+// Airplane Constructor
+const Airplane = function (name) {
+  FlyingVehicle.call(this, name);
+};
+
+Airplane.prototype = new FlyingVehicle();
+
+Airplane.prototype.barrelRoll = () => {
+  return 'Do a barrel roll!';
+};
+
+Airplane.prototype.surelyYouCantBeSerious = () => {
+  return 'I am serious. And don\'t call me Shirley';
+};
+
+// Helicopter Constructor
+const Helicopter = function (name) {
+  FlyingVehicle.call(this, name);
+};
+
+Helicopter.prototype = new FlyingVehicle;
+
+Helicopter.prototype.decapitate = () => {
+  return 'Duck!';
+};
+
+module.exports = {Airplane, Helicopter};
+
+// let heli = new Airplane('hey'); //?
+
+// heli.surelyYouCantBeSerious(); //?
+// heli.canFly; //?
+// heli.name; //?
+// heli.barrelRoll(); //?
+// heli.crash(); //?
--- objects/index.js
+++ objects/index.js
@@ -4,23 +4,53 @@ const VehicleConstructor = require('./vehicle-constructor.js');
 const VehicleClass = require('./vehicle-class.js');
 const VehicleFactory = require('./vehicle-factory.js');
 
-// Implement a car and motorcycle using a Constructor
-const mazda = new VehicleConstructor.Car('Mazda 3');
-console.log(mazda.name, mazda.drive(), mazda.stop());
+const FlyingVehicleConstructor = require('./flyingvehicle-constructor.js');
+const FlyingVehicleFactory = require('./flyingvehicle-factory.js');
 
-const harley = new VehicleConstructor.Motorcycle('Harley');
-console.log(harley.name, harley.wheelie(), harley.stop());
 
-// Implement a car and motorcycle using a Class
-const nissan = new VehicleClass.Car('Nissan Versa');
-console.log(nissan.name, nissan.drive(), nissan.stop());
 
-const yamaha = new VehicleClass.Motorcycle('Yamaha');
-console.log(yamaha.name, yamaha.wheelie(), yamaha.stop());
+// Vehicles
+// Implement a airplane and motorcycle using a Constructor
+// const mazda = new VehicleConstructor.Car('Mazda 3');
+// console.log(mazda.name, mazda.drive(), mazda.stop());
 
-// Implement a car and motorcycle using a Factory
-const toyota = new VehicleFactory.CarFactory('Toyota Camry');
-console.log(toyota.name, toyota.drive(), toyota.stop());
+// const harley = new VehicleConstructor.Motorcycle('Harley');
+// console.log(harley.name, harley.wheelie(), harley.stop());
 
-const ducati = new VehicleFactory.MotorcycleFactory('Ducati');
-console.log(ducati.name, ducati.wheelie(), ducati.stop());
+// // Implement a car and motorcycle using a Class
+// const nissan = new VehicleClass.Car('Nissan Versa');
+// console.log(nissan.name, nissan.drive(), nissan.stop());
+
+// const yamaha = new VehicleClass.Motorcycle('Yamaha');
+// console.log(yamaha.name, yamaha.wheelie(), yamaha.stop());
+
+// // Implement a car and motorcycle using a Factory
+// const toyota = new VehicleFactory.CarFactory('Toyota Camry');
+// console.log(toyota.name, toyota.drive(), toyota.stop());
+
+// const ducati = new VehicleFactory.MotorcycleFactory('Ducati');
+// console.log(ducati.name, ducati.wheelie(), ducati.stop());
+
+
+
+// FlyingVehicles
+// Implement a airplane and helicopter using a Constructor
+const boeing747 = new FlyingVehicleConstructor.Airplane('Boeing 747');
+console.log(boeing747.name, boeing747.takeOff(), boeing747.crash(), boeing747.barrelRoll(), boeing747.surelyYouCantBeSerious());
+
+const blackhawk = new FlyingVehicleConstructor.Helicopter('Blackhawk');
+console.log(blackhawk.name, blackhawk.takeOff(), blackhawk.crash(), blackhawk.decapitate());
+
+// // Implement a airplane and helicopter using a Class
+const gSixPrivate = new FlyingVehicleConstructor.Airplane('Gulfstream G650');
+console.log(gSixPrivate.name, gSixPrivate.takeOff(), gSixPrivate.crash(), gSixPrivate.barrelRoll(), gSixPrivate.surelyYouCantBeSerious());
+
+const apache = new FlyingVehicleConstructor.Helicopter('Boeing AH-64 Apache');
+console.log(apache.name, apache.takeOff(), apache.crash(), apache.decapitate());
+
+// // Implement a airplane and helicopter using a Factory
+// const toyota = new FlyingVehicleFactory.AirplaneFactory('Toyota Camry');
+// console.log(toyota.name, toyota.drive(), toyota.crash());
+
+// const ducati = new FlyingVehicleFactory.HelicopterFactory('Ducati');
+// console.log(ducati.name, ducati.takeOff(), ducati.crash());
